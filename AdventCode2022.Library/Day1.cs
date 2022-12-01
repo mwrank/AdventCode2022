ba70c8f6ddd61f291948c14951a01ed5bfa72077
mattr <mattr@DESKTOP-G8OOH40>
2022-12-01 17:52:04 -0500

    Add project files.

---
+++ AdventCode2022.Library/Day1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCode2022.Library
+{
+    public static class Day1
+    {
+        /// <summary>
+        /// Advent code day 1 part 1
+        /// </summary>
+        /// <param name="filePath">Path to the file</param>
+        /// <returns>Sum of calories for the elf carrying the most calories</returns>
+        public static int Part1(string filePath)
+        {
+            List<Elf> elves = new List<Elf>();
+            Elf elf = new Elf();
+
+            foreach (string line in System.IO.File.ReadLines(filePath))
+            {
+                if(!string.IsNullOrEmpty(line))
+                {
+                    int snack = int.Parse(line);
+                    elf.Snacks.Add(snack);
+                }
+                else
+                {
+                    elves.Add(elf);
+                    elf = new Elf();
+                }
+            }
+
+            elves.Add(elf);
+            return elves.OrderByDescending(x => x.TotalCalories).First().TotalCalories;
+        }
+
+        /// <summary>
+        /// Advent code day 1 part 2
+        /// </summary>
+        /// <param name="filePath">Path to the file</param>
+        /// <returns>Sum of calories for the top three elves carrying the most calories</returns>
+        public static int Part2(string filePath)
+        {
+            List<Elf> elves = new List<Elf>();
+            Elf elf = new Elf();
+
+            foreach (string line in System.IO.File.ReadLines(filePath))
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    int snack = int.Parse(line);
+                    elf.Snacks.Add(snack);
+                }
+                else
+                {
+                    elves.Add(elf);
+                    elf = new Elf();
+                }
+            }
+
+            elves.Add(elf);
+            return elves.OrderByDescending(x => x.TotalCalories).Take(3).Sum(x => x.TotalCalories);
+        }
+    }
+
+    internal class Elf
+    {
+        public List<int> Snacks { get; set; }
+
+        public int TotalCalories
+        {
+            get { return Snacks.Sum(); }
+        }
+
+        public Elf()
+        {
+            Snacks = new List<int>();
+        }
+    }
+}
---
+++ AdventCode2022.Tests/AdventTests.cs
@@ -0,0 +1,29 @@
+using AdventCode2022.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2022.Tests
+{
+    [TestClass]
+    public class AdventTests
+    {
+        #region Day1
+        [TestMethod]
+        public void Day1_Part1()
+        {
+            string path = @"Data\Day1\sample.txt";
+            int result = Day1.Part1(path);
+
+            Assert.AreEqual(result, 24000);
+        }
+
+        [TestMethod]
+        public void Day1_Part2()
+        {
+            string path = @"Data\Day1\sample.txt";
+            int result = Day1.Part2(path);
+
+            Assert.AreEqual(result, 45000);
+        } 
+        #endregion
+    }
+}
